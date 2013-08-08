99f4682a420f222eab1b66cf7d71bedfca9ef2d4
randell <josephrandell.benavidez@gmail.com>
2013-08-08 23:03:07 +1000

    Committed solutions to Problem 1 of Project Euler: Multiples 3 and 5. First solution is a naive solution using a loop and modulus division. Second solution utilizes the sum of arithmetic progression.

---
+++ 0001-multiples_of_3_or_5-b.py
@@ -0,0 +1,32 @@
+"""
+Project Euler Problem 1
+
+Title:
+Multiples of 3 or 5
+
+Description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
+multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Solution:
+This version utilizes the formula for getting the sum of an arithmetic series.
+See http://en.wikipedia.org/wiki/Arithmetic_progression.
+
+Answer: 233168.
+I'm the 311384th person to solve this problem.
+
+"""
+count_multiples_of_3 = 999 / 3
+count_multiples_of_5 = 999 / 5
+count_multiples_of_15 = 999 / 15
+
+def sum_of_arithmetic_series(n, a, d):
+    return n * ((2 * a) + d * (n-1)) / 2
+
+sum_3 = sum_of_arithmetic_series(count_multiples_of_3, 3, 3)
+sum_5 = sum_of_arithmetic_series(count_multiples_of_5, 5, 5)
+sum_15 = sum_of_arithmetic_series(count_multiples_of_15, 15, 15)
+
+print sum_3 + sum_5 - sum_15
---
+++ 0001-multiples_of_3_or_5.py
@@ -0,0 +1,24 @@
+"""
+Project Euler Problem 1
+
+Title:
+Multiples of 3 or 5
+
+Description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
+multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Solution:
+This version is a naive implementation using a loop and modulus division.
+
+Answer: 233168.
+I'm the 311384th person to solve this problem.
+"""
+sum = 0
+
+for x in range(0, 10):
+    sum += x if x % 3 == 0  or x % 5 == 0 else 0
+
+print sum
