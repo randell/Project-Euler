0916064538b3e40e78918713d76bbcdefdc804c5
randell <josephrandell.benavidez@gmail.com>
2013-08-23 13:49:25 +1000

    Added solution to Problem 10: Summation of primes.

---
+++ 0010-summation_of_primes-a.py
@@ -0,0 +1,33 @@
+"""
+Project Euler Problem 10
+
+Title:
+Summation of primes
+
+Description:
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+Solution:
+Just sum up all the primes below two million.
+
+Answer: 142913828922
+I'm the 129964th person to solve this problem.
+Earned Decathlete: Solve ten consecutive problems
+
+"""
+from lib.util import *
+
+x = 2000000
+
+def sum_of_primes_below(x):
+    sum = 0
+
+    for i in range(2, x):
+        if is_prime(i):
+            sum += i
+
+    return sum
+
+print sum_of_primes_below(x)
