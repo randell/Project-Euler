f13c03339e5c2ceb0a8b97aa0b76d3401d20c418
randell <randell.benavidez@readingroom.com.au>
2015-06-25 22:56:05 +1000

    Added solution to Problem 25: 1000-digit Fibonacci number

--- 0025-1000_digit_fibonacci_number.py
+++ 0025-1000_digit_fibonacci_number.py
@@ -31,7 +31,30 @@ digits?
 Solution:
 
 
-Answer:
-I'm the Nth person to solve this problem.
+Answer: 4782
+I'm the 92,572nd  person to solve this problem.
+
+Nice work, randell.benavidez, you've just advanced to Level 1.
+72997 members (14.62%) have made it this far.
+
+You have earned 1 new award:
+
+The Journey Begins: Progress to Level 1 by solving twenty-five problems
 
 """
+a = 1
+b = 1
+c = 0
+counter = 2
+digits = 1000
+
+while True:
+    counter += 1
+    c = a + b
+
+    a = b
+    b = c
+    
+    if len(str(c)) == 1000:
+        print counter
+        break
